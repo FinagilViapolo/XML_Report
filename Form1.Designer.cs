56f846c94e861e10a2ccdeeb3a4ac7631373fe96
viapolo <viapolo@finagil.com.mx>
2018-02-07 19:41:59 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace XML_Report
+{
+    partial class frmReporte
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReporte = new System.Windows.Forms.DataGridView();
+            this.fdBwCargaXML = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbValidaSAT = new System.Windows.Forms.CheckBox();
+            this.btnReporteXLS = new System.Windows.Forms.Button();
+            this.pbEstatusProceso = new System.Windows.Forms.ProgressBar();
+            this.svfReporteXLS = new System.Windows.Forms.SaveFileDialog();
+            this.dgCons = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgSerie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgFolio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgVersion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgRFCEmi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgRFCRec = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgISR = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgFecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgUUID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgFechaIPago = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgFechaFPago = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgTPercepciones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgTDeducciones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgEstatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgVersionN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReporte
+            // 
+            this.dgvReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporte.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgCons,
+            this.dgSerie,
+            this.dgFolio,
+            this.dgVersion,
+            this.dgRFCEmi,
+            this.dgRFCRec,
+            this.dgISR,
+            this.dgFecha,
+            this.dgUUID,
+            this.dgTotal,
+            this.dgFechaIPago,
+            this.dgFechaFPago,
+            this.dgTPercepciones,
+            this.dgTDeducciones,
+            this.dgEstatus,
+            this.dgVersionN});
+            this.dgvReporte.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvReporte.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvReporte.Location = new System.Drawing.Point(0, 0);
+            this.dgvReporte.Name = "dgvReporte";
+            this.dgvReporte.ReadOnly = true;
+            this.dgvReporte.Size = new System.Drawing.Size(937, 315);
+            this.dgvReporte.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(862, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cargar XML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbValidaSAT
+            // 
+            this.cbValidaSAT.AutoSize = true;
+            this.cbValidaSAT.Location = new System.Drawing.Point(727, 325);
+            this.cbValidaSAT.Name = "cbValidaSAT";
+            this.cbValidaSAT.Size = new System.Drawing.Size(120, 17);
+            this.cbValidaSAT.TabIndex = 2;
+            this.cbValidaSAT.Text = "Validar Estatus SAT";
+            this.cbValidaSAT.UseVisualStyleBackColor = true;
+            // 
+            // btnReporteXLS
+            // 
+            this.btnReporteXLS.Location = new System.Drawing.Point(534, 321);
+            this.btnReporteXLS.Name = "btnReporteXLS";
+            this.btnReporteXLS.Size = new System.Drawing.Size(102, 23);
+            this.btnReporteXLS.TabIndex = 3;
+            this.btnReporteXLS.Text = "Reporte XLS";
+            this.btnReporteXLS.UseVisualStyleBackColor = true;
+            this.btnReporteXLS.Click += new System.EventHandler(this.btnReporteXLS_Click);
+            // 
+            // pbEstatusProceso
+            // 
+            this.pbEstatusProceso.Location = new System.Drawing.Point(240, 161);
+            this.pbEstatusProceso.Name = "pbEstatusProceso";
+            this.pbEstatusProceso.Size = new System.Drawing.Size(493, 23);
+            this.pbEstatusProceso.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pbEstatusProceso.TabIndex = 4;
+            this.pbEstatusProceso.Visible = false;
+            // 
+            // dgCons
+            // 
+            this.dgCons.HeaderText = "No";
+            this.dgCons.Name = "dgCons";
+            // 
+            // dgSerie
+            // 
+            this.dgSerie.HeaderText = "Serie";
+            this.dgSerie.Name = "dgSerie";
+            // 
+            // dgFolio
+            // 
+            this.dgFolio.HeaderText = "Folio";
+            this.dgFolio.Name = "dgFolio";
+            // 
+            // dgVersion
+            // 
+            this.dgVersion.HeaderText = "Versión";
+            this.dgVersion.Name = "dgVersion";
+            // 
+            // dgRFCEmi
+            // 
+            this.dgRFCEmi.HeaderText = "RFC Emi";
+            this.dgRFCEmi.Name = "dgRFCEmi";
+            // 
+            // dgRFCRec
+            // 
+            this.dgRFCRec.HeaderText = "RFC Rec";
+            this.dgRFCRec.Name = "dgRFCRec";
+            // 
+            // dgISR
+            // 
+            this.dgISR.HeaderText = "ISR";
+            this.dgISR.Name = "dgISR";
+            // 
+            // dgFecha
+            // 
+            this.dgFecha.HeaderText = "Fecha";
+            this.dgFecha.Name = "dgFecha";
+            // 
+            // dgUUID
+            // 
+            this.dgUUID.HeaderText = "UUID";
+            this.dgUUID.Name = "dgUUID";
+            // 
+            // dgTotal
+            // 
+            this.dgTotal.HeaderText = "Total";
+            this.dgTotal.Name = "dgTotal";
+            // 
+            // dgFechaIPago
+            // 
+            this.dgFechaIPago.HeaderText = "Fecha In Pago";
+            this.dgFechaIPago.Name = "dgFechaIPago";
+            // 
+            // dgFechaFPago
+            // 
+            this.dgFechaFPago.HeaderText = "Fecha Fin Pago";
+            this.dgFechaFPago.Name = "dgFechaFPago";
+            // 
+            // dgTPercepciones
+            // 
+            this.dgTPercepciones.HeaderText = "Total Percepciones";
+            this.dgTPercepciones.Name = "dgTPercepciones";
+            // 
+            // dgTDeducciones
+            // 
+            this.dgTDeducciones.HeaderText = "Total Deducciones";
+            this.dgTDeducciones.Name = "dgTDeducciones";
+            // 
+            // dgEstatus
+            // 
+            this.dgEstatus.HeaderText = "Estatus SAT";
+            this.dgEstatus.Name = "dgEstatus";
+            // 
+            // dgVersionN
+            // 
+            this.dgVersionN.HeaderText = "Versión Nómina";
+            this.dgVersionN.Name = "dgVersionN";
+            // 
+            // frmReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 347);
+            this.Controls.Add(this.pbEstatusProceso);
+            this.Controls.Add(this.btnReporteXLS);
+            this.Controls.Add(this.cbValidaSAT);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvReporte);
+            this.Name = "frmReporte";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XML Report";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReporte;
+        private System.Windows.Forms.FolderBrowserDialog fdBwCargaXML;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox cbValidaSAT;
+        private System.Windows.Forms.Button btnReporteXLS;
+        private System.Windows.Forms.ProgressBar pbEstatusProceso;
+        private System.Windows.Forms.SaveFileDialog svfReporteXLS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgCons;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgSerie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgFolio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgVersion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgRFCEmi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgRFCRec;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgISR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgFecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgUUID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgFechaIPago;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgFechaFPago;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgTPercepciones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgTDeducciones;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgEstatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgVersionN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace XML_Report
+{
+    public partial class frmReporte : Form
+    {
+        public string dir_raiz = "";
+        public int no_archivos = 0;
+        public frmReporte()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fdBwCargaXML.ShowDialog() == DialogResult.OK)
+            {
+                dir_raiz = fdBwCargaXML.SelectedPath + "\\";
+                string[] dirs = Directory.GetFiles(dir_raiz, "*.xml");
+                no_archivos = Convert.ToInt32(dirs.Length);
+            }
+            pbEstatusProceso.Visible = true;
+            Load_XML(dir_raiz);
+            pbEstatusProceso.Visible = false;
+        }
+
+        public void Load_XML(string dir)
+        {
+            pbEstatusProceso.Value = 1;
+            pbEstatusProceso.Step = 1;
+            pbEstatusProceso.Maximum = no_archivos+1;
+            dgvReporte.Rows.Clear();
+            try
+            {
+                DirectoryInfo d = new DirectoryInfo(dir);
+            
+            int fila = 0;
+            
+                foreach (FileInfo file in d.GetFiles("*.xml"))
+                {
+                    XmlDataDocument doc = new XmlDataDocument();
+                    doc.Load(dir + file.Name.ToString());
+
+                    XmlNode node;
+                    node = doc.DocumentElement;
+                    string rfc_emisor = "", rfc_receptor = "", total = "", uuid = "";
+
+                    dgvReporte.Rows.Add();
+                    dgvReporte.Rows[fila].Cells[0].Value = (fila+1).ToString();
+                    foreach (XmlNode node1 in node.Attributes)
+                    {
+                        if (node1.Name == "Serie" || node1.Name == "serie")
+                        {
+                            dgvReporte.Rows[fila].Cells[1].Value = node1.Value.ToString();
+                        }
+                        if (node1.Name == "Folio" || node1.Name == "folio")
+                        {
+                            dgvReporte.Rows[fila].Cells[2].Value = node1.Value.ToString();
+                        }
+                        if (node1.Name == "Version" || node1.Name == "version")
+                        {
+                            dgvReporte.Rows[fila].Cells[3].Value = node1.Value.ToString();
+                        }
+                        if (node1.Name == "Fecha" || node1.Name == "fecha")
+                        {
+                            dgvReporte.Rows[fila].Cells[7].Value = Convert.ToDateTime(node1.Value.ToString());
+                        }
+                        if (node1.Name == "Total" || node1.Name == "total")
+                        {
+                            dgvReporte.Rows[fila].Cells[9].Value = node1.Value.ToString();
+                            total = node1.Value.ToString();
+                        }
+                        foreach (XmlNode node1_a in node.ChildNodes)
+                        {
+                            if (node1_a.Name == "cfdi:Emisor")
+                            {
+                                foreach (XmlNode node1_aa in node1_a.Attributes)
+                                {
+                                    if (node1_aa.Name == "Rfc" || node1_aa.Name == "rfc")
+                                    {
+                                        dgvReporte.Rows[fila].Cells[4].Value = node1_aa.Value.ToString();
+                                        rfc_emisor = node1_aa.Value.ToString();
+                                    }
+                                }
+                            }
+                        }
+                        foreach (XmlNode node1_a in node.ChildNodes)
+                        {
+                            if (node1_a.Name == "cfdi:Receptor")
+                            {
+                                foreach (XmlNode node1_aa in node1_a.Attributes)
+                                {
+                                    if (node1_aa.Name == "Rfc" || node1_aa.Name == "rfc")
+                                    {
+                                        dgvReporte.Rows[fila].Cells[5].Value = node1_aa.Value.ToString();
+                                        rfc_receptor = node1_aa.Value.ToString();
+                                    }
+                                }
+                            }
+                        }
+                        foreach (XmlNode node1_a in node.ChildNodes)
+                        {
+                            if (node1_a.Name == "cfdi:Complemento")
+                            {
+                                foreach (XmlNode node1_aa in node1_a.ChildNodes)
+                                {
+                                    if (node1_aa.Name == "nomina12:Nomina" || node1_aa.Name == "nomina:Nomina")
+                                    {
+                                        foreach (XmlNode node1_at in node1_aa.Attributes)
+                                        {
+                                            if (node1_at.Name == "FechaInicialPago")
+                                            {
+                                                dgvReporte.Rows[fila].Cells[10].Value = Convert.ToDateTime(node1_at.Value.ToString());
+                                            }
+                                            if (node1_at.Name == "FechaFinalPago")
+                                            {
+                                                dgvReporte.Rows[fila].Cells[11].Value = Convert.ToDateTime(node1_at.Value.ToString());
+                                            }
+                                            if (node1_at.Name == "TotalPercepciones")
+                                            {
+                                                dgvReporte.Rows[fila].Cells[12].Value = node1_at.Value.ToString();
+                                            }
+                                            if (node1_at.Name == "TotalDeducciones")
+                                            {
+                                                dgvReporte.Rows[fila].Cells[13].Value = node1_at.Value.ToString();
+                                            }
+                                            if (node1_at.Name == "Version")
+                                            {
+                                                dgvReporte.Rows[fila].Cells[15].Value = node1_at.Value.ToString();
+                                            }
+                                        }
+                                        foreach (XmlNode node1_ab in node1_aa.ChildNodes)
+                                        {
+                                            if (node1_ab.Name == "nomina12:Deducciones")
+                                            {
+                                                foreach (XmlNode node1_ac in node1_ab.ChildNodes)
+                                                {
+                                                    if (node1_ac.Name == "nomina12:Deduccion")
+                                                    {
+                                                        foreach (XmlNode node1_ad in node1_ac.Attributes)
+                                                        {
+                                                            if (node1_ad.Name == "TipoDeduccion")
+                                                            {
+                                                                if (node1_ad.Value == "002")
+                                                                {
+                                                                    //dgvReporte.Rows[fila].Cells[6].Value = node1_ad.Value.ToString();
+                                                                    foreach (XmlNode node1_impo in node1_ac.Attributes)
+                                                                    {
+                                                                        if (node1_impo.Name == "Importe")
+                                                                        {
+                                                                            dgvReporte.Rows[fila].Cells[6].Value = node1_impo.Value.ToString();
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (node1_aa.Name == "tfd:TimbreFiscalDigital")
+                                    {
+                                        foreach (XmlNode node1_ab in node1_aa.Attributes)
+                                        {
+                                            if (node1_ab.Name == "UUID")
+                                            {
+                                                dgvReporte.Rows[fila].Cells[8].Value = node1_ab.Value.ToString();
+                                                uuid = node1_ab.Value.ToString();
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (cbValidaSAT.Checked == true)
+                    {
+                        WS_SAT.ConsultaCFDIServiceClient servicio = new WS_SAT.ConsultaCFDIServiceClient();
+                        dgvReporte.Rows[fila].Cells[14].Value = servicio.Consulta("?re=" + rfc_emisor + "&rr=" + rfc_receptor + "&tt=" + total + "&id=" + uuid).Estado.ToString();
+                        frmReporte.ActiveForm.Update();
+                    }
+                    pbEstatusProceso.Value += 1;
+                    ++fila;
+                }
+            }
+            catch(Exception ex){
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnReporteXLS_Click(object sender, EventArgs e)
+        {
+            pbEstatusProceso.Value = 1;
+            pbEstatusProceso.Step = 1;
+            pbEstatusProceso.Maximum = no_archivos + 5;
+            pbEstatusProceso.Visible = true;
+            //Excel
+            Excel.Application App_Excel = default(Excel.Application);
+            Excel.Workbook LibroExcel = default(Excel.Workbook);
+            Excel.Worksheet HojaExcel = default(Excel.Worksheet);
+
+            App_Excel = new Excel.Application();
+                        LibroExcel = App_Excel.Workbooks.Add();
+            HojaExcel = LibroExcel.Worksheets[1];
+            HojaExcel.Visible = Excel.XlSheetVisibility.xlSheetVisible;
+
+            HojaExcel.Activate();
+
+            HojaExcel.Range["A1:E1"].Font.Bold = true;
+            HojaExcel.Range["A1:E1"].Font.Size = 15;
+
+            HojaExcel.Range["E2:J2"].Merge();
+            HojaExcel.Range["E2:J2"].Value = "REPORTE DE COMPROBANTES FISCALES DIGITALES";
+            HojaExcel.Range["E2:J2"].Font.Bold = true;
+            HojaExcel.Range["E2:J2"].Font.Size = 15;
+
+            Excel.Range objCelda = HojaExcel.Range["A3", Type.Missing];
+            objCelda.Value = "Consecutivo";
+
+            objCelda = HojaExcel.Range["B3", Type.Missing];
+            objCelda.Value = "Serie";
+
+            objCelda = HojaExcel.Range["C3", Type.Missing];
+            objCelda.Value = "Folio";
+            objCelda.EntireColumn.NumberFormat = "###";
+
+            objCelda = HojaExcel.Range["D3", Type.Missing];
+            objCelda.Value = "Versión";
+
+            objCelda = HojaExcel.Range["E3", Type.Missing];
+            objCelda.Value = "RFC Emisor";
+
+            objCelda = HojaExcel.Range["F3", Type.Missing];
+            objCelda.Value = "RFC Receptor";
+
+            objCelda = HojaExcel.Range["G3", Type.Missing];
+            objCelda.Value = "ISR";
+
+            objCelda = HojaExcel.Range["H3", Type.Missing];
+            objCelda.Value = "Fecha";
+
+            objCelda = HojaExcel.Range["I3", Type.Missing];
+            objCelda.Value = "UUID";
+            
+            objCelda = HojaExcel.Range["J3", Type.Missing];
+            objCelda.Value = "Total";
+
+            objCelda = HojaExcel.Range["K3", Type.Missing];
+            objCelda.Value = "Fecha I Pago";
+
+            objCelda = HojaExcel.Range["L3", Type.Missing];
+            objCelda.Value = "Fecha F Pago";
+
+            objCelda = HojaExcel.Range["M3", Type.Missing];
+            objCelda.Value = "Total Percepciones";
+
+            objCelda = HojaExcel.Range["N3", Type.Missing];
+            objCelda.Value = "Total Deducciones";
+
+            objCelda = HojaExcel.Range["O3", Type.Missing];
+            objCelda.Value = "Estatus SAT";
+
+            objCelda = HojaExcel.Range["P3", Type.Missing];
+            objCelda.Value = "Versión N";
+
+            int i = 4;
+            foreach(DataGridViewRow Row in dgvReporte.Rows)
+                {
+                // Asignar los valores de los registros a las celdas
+                HojaExcel.Cells[i, "A"] = Row.Cells[0].Value;
+                HojaExcel.Cells[i, "B"] = Row.Cells[1].Value;
+                HojaExcel.Cells[i, "C"] = Row.Cells[2].Value;
+                HojaExcel.Cells[i, "D"] = Row.Cells[3].Value;
+                HojaExcel.Cells[i, "E"] = Row.Cells[4].Value;
+                HojaExcel.Cells[i, "F"] = Row.Cells[5].Value;
+                HojaExcel.Cells[i, "G"] = Row.Cells[6].Value;
+                HojaExcel.Cells[i, "H"] = Row.Cells[7].Value;
+                HojaExcel.Cells[i, "I"] = Row.Cells[8].Value;
+                HojaExcel.Cells[i, "J"] = Row.Cells[9].Value;
+                HojaExcel.Cells[i, "K"] = Row.Cells[10].Value;
+                HojaExcel.Cells[i, "L"] = Row.Cells[11].Value;
+                HojaExcel.Cells[i, "M"] = Row.Cells[12].Value;
+                HojaExcel.Cells[i, "N"] = Row.Cells[13].Value;
+                HojaExcel.Cells[i, "O"] = Row.Cells[14].Value;
+                HojaExcel.Cells[i, "P"] = Row.Cells[15].Value;
+
+                pbEstatusProceso.Value += 1;
+                i++;
+            }
+            App_Excel.Visible = true;
+            pbEstatusProceso.Visible = false;
+            //Termina Excel
+        }
+    }
+}
